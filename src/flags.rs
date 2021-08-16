63ba6cf6998c428ab3c202da2436da0ecc98107b
Nicolas BAUW <nbauw@hotmail.com>
2021-08-16 22:35:29 +0200

    Rename struct fields

--- src/flags.rs
+++ src/flags.rs
@@ -1,19 +1,19 @@
 pub struct Flags {
-    pub sign: bool,                 // bit 7
-    pub zero: bool,                 // bit 6
-    pub auxiliary_carry: bool,      // bit 4
-    pub parity: bool,               // bit 2
-    pub carry: bool                 // bit 0
+    pub s: bool,                // sign             : bit 7
+    pub z: bool,                // zero             : bit 6
+    pub a: bool,                // auxiliary carry  : bit 4
+    pub p: bool,                // parity           : bit 2
+    pub c: bool                 // carry            : bit 0
 }
 
 impl Flags {
     pub fn new() -> Flags {
         Flags {
-            sign: false,
-            zero: false,
-            auxiliary_carry: false,
-            parity: false,
-            carry: false
+            s: false,
+            z: false,
+            a: false,
+            p: false,
+            c: false
         }
     }
 }
