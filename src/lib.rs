791142c792b9a51928c20840a83b82c2c6770c07
Nicolas BAUW <nbauw@hotmail.com>
2021-08-20 18:04:12 +0200

    jz & jnz

--- src/lib.rs
+++ src/lib.rs
@@ -737,12 +737,22 @@ impl CPU {
             0xD2 => {                                                       // JNC
                 let addr = self.bus.read_word(self.pc + 1);
                 if !self.flags.c { self.pc = addr; }
+            },
+            // JZ Jump if carry
+            0xCA => {                                                       // JZ
+                let addr = self.bus.read_word(self.pc + 1);
+                if self.flags.z { self.pc = addr; }
+            },
+            // JNZ Jump if carry
+            0xC2 => {                                                       // JNZ
+                let addr = self.bus.read_word(self.pc + 1);
+                if !self.flags.z { self.pc = addr; }
             }
             _ => {}
         }
 
         match opcode {
-            0xe9 | 0xc3 | 0xDA | 0xD2 => {},
+            0xe9 | 0xc3 | 0xDA | 0xD2 | 0xCA | 0xC2 => {},
             0x06 | 0x0E | 0x16 | 0x1E | 0x26 | 0x2E | 0x36 | 0x3E |
             0xC6 | 0xCE | 0xD6 | 0xDE | 0xE6 | 0xEE | 0xF6 | 0xFE => self.pc += 2,
             0x32 | 0x3A | 0x22 | 0x2A => self.pc += 3,
