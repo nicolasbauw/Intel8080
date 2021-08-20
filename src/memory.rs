a5e8af8c2b109a19717170b317ac79b4c42b7f2d
Nicolas BAUW <nbauw@hotmail.com>
2021-08-20 08:56:15 +0200

    word read/write functions

--- src/memory.rs
+++ src/memory.rs
@@ -16,6 +16,15 @@ impl AddressBus {
     pub fn write_byte(&mut self, address: u16, data: u8) {
         self.ram[usize::from(address)] = data;
     }
+
+    pub fn read_word(&self, address: u16) -> u16 {
+        u16::from(self.ram[usize::from(address)]) | (u16::from(self.ram[usize::from(address + 1)]) << 8)
+    }
+
+    pub fn write_word(&mut self, address: u16, data: u16) {
+        self.ram[usize::from(address)] = (data & 0xFF) as u8;
+        self.ram[usize::from(address + 1)] = (data >> 8) as u8;
+    }
 }
 
 #[cfg(test)]
@@ -26,5 +35,12 @@ mod tests {
         let mut b = AddressBus::new();
         b.write_byte(0x0000, 0xFF);
         assert_eq!(b.read_byte(0x0000), 0xFF);
+    }
+
+    #[test]
+    fn rw_word() {
+        let mut b = AddressBus::new();
+        b.write_word(0x0000, 0x1be3);
+        assert_eq!(b.read_word(0x0000), 0x1be3);
     }
 }
