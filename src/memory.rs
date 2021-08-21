e64a2b6f6ac1a208172a533f8d0ef99ea8ed71cd
Nicolas BAUW <nbauw@hotmail.com>
2021-08-21 21:58:27 +0200

    load binary data function

--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::prelude::*,};
+
 pub struct AddressBus {
     ram: Vec<u8>
 }
@@ -24,6 +26,15 @@ impl AddressBus {
     pub fn write_word(&mut self, address: u16, data: u16) {
         self.ram[usize::from(address)] = (data & 0xFF) as u8;
         self.ram[usize::from(address + 1)] = (data >> 8) as u8;
+    }
+
+    // Loads binary data into memory at $0000 + offset
+    pub fn load_bin(&mut self, file: &str, org: u16) -> Result<(), std::io::Error> {
+        let mut f = File::open(file)?;
+        let mut buf = Vec::new();
+        f.read_to_end(&mut buf)?;
+        self.ram[org as usize..(buf.len() + org as usize)].clone_from_slice(&buf[..]);
+        Ok(())
     }
 }
 
