3f3b363f73458d6d696dc45037ff2072fdfe6975
Nicolas BAUW <nbauw@hotmail.com>
2021-08-21 22:23:33 +0200

    ret -> hlt

--- examples/helloworld.asm
+++ examples/helloworld.asm
@@ -1,11 +1,11 @@
  .target "8080"
- .format "txt"
+ .format "bin"
  .org $100
  
 bdos    .equ    $0005   ; BDOS entry point
 start:  mvi    c,9      ; BDOS function: output string
         lxi    d,msg    ; address of msg
         call   bdos
-        ret             ; return to CCP
+        hlt             ; Stops execution
 msg    .text   "Hello, world!"
        .end
