6184ac87c95097b1b540390fa07129f330c1e010
Nicolas BAUW <nbauw@hotmail.com>
2021-08-21 22:00:35 +0200

    helloworld example for CPM (retroassembler source)

---
+++ examples/helloworld.asm
@@ -0,0 +1,11 @@
+ .target "8080"
+ .format "txt"
+ .org $100
+ 
+bdos    .equ    $0005   ; BDOS entry point
+start:  mvi    c,9      ; BDOS function: output string
+        lxi    d,msg    ; address of msg
+        call   bdos
+        ret             ; return to CCP
+msg    .text   "Hello, world!"
+       .end
