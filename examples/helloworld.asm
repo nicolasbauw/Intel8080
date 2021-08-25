fc1f85802f6de63e99c9199d96e356bf090bd7ce
Nicolas BAUW <nbauw@hotmail.com>
2021-08-25 21:21:42 +0200

    setting up stack

--- examples/helloworld.asm
+++ examples/helloworld.asm
@@ -1,11 +1,12 @@
  .target "8080"
  .format "bin"
- .org $100              ; Useless : "bin" format has no header
+ .org $100                      ; Useless : "bin" format has no header
  
-bdos    .equ    $0005   ; BDOS entry point
-start:  mvi    c,9      ; BDOS function: output string
-        lxi    d,msg    ; address of msg
-        call   bdos
-        ret             ; return to CCP
-msg    .text   "Hello, world!$"
+bdos    .equ    $0005           ; BDOS entry point
+        lxi     sp, $FF00       ; setting up stack
+start:  mvi     c,9             ; BDOS function: output string
+        lxi     d,msg           ; address of msg
+        call    bdos
+        ret                     ; return to CCP
+msg    .text    "Hello, world!$"
        .end
