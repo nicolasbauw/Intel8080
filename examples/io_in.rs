c33f20e2c567e2110016bc08dd0fbd9d29cbb772
Nicolas BAUW <nbauw@hotmail.com>
2022-10-15 10:01:41 +0200

    io_req channel

--- examples/io_in.rs
+++ examples/io_in.rs
@@ -1,4 +1,4 @@
-use std::{ error::Error, process, time::Duration, thread };
+use std::{ error::Error, process, thread };
 use intel8080::CPU;
 
 fn main() {
@@ -16,11 +16,23 @@ fn load_execute() -> Result<(), Box<dyn Error>> {
     c.bus.load_bin("bin/in_a.bin", 0)?;
 
     // io.0 is the sender, io.1 is the receiver. Used to send / receive a (device, data) tuple to / from a peripheral.
-    let io_sender1 = c.bus.io_in.0.clone();
+    let periph1_sender = c.bus.io_in.0.clone();
+    let periph1_req_receiver = c.bus.io_req.1.clone();
 
-    // In this example periph is the entry function that simulates a peripheral. It runs in a separate thread.
+    // Demonstration peripheral 0x07 sends the 0xDE message when a IN instruction occurs
     thread::spawn(move || {
-        periph(io_sender1);
+        // IN instruction automatically sends a request message through the io_req channel
+        // So the peripheral knows when he can send the message to the CPU via the io_in sender
+        loop {
+            if let Ok(device) = periph1_req_receiver.recv() {
+                // IN instruction for the 0x07 device ?
+                if device == 0x07 {
+                    println!("The 0x07 peripheral puts 0xDE on the data bus");
+                    // We send the data through the io_in channel
+                    periph1_sender.send((0x07, 0xDE)).unwrap();
+                }
+            }
+        }
     });
 
     // A single loop which waits for the 0xDE byte to be sent by the 0x07 peripheral
@@ -29,13 +41,4 @@ fn load_execute() -> Result<(), Box<dyn Error>> {
         if c.pc == 0x0000 { break }
     }
     Ok(())
-}
-
-// Demonstration peripheral 0x07 sends 0xDE on the data bus after 1 second
-fn periph(tx: crossbeam_channel::Sender<(u8, u8)>) {
-    loop {
-        std::thread::sleep(Duration::from_secs(1));
-        println!("The 0x07 peripheral puts 0xDE on the data bus");
-        tx.send((0x07, 0xDE)).unwrap();
-    }
 }
